ea6faf5a2841e7516130838f5295cd8f292827cd
OmSadigale <163130963+OmSadigale@users.noreply.github.com>
2024-05-07 10:56:59 +0100

    Final

--- Dashboard.py
+++ Dashboard.py
@@ -52,38 +52,113 @@ st.markdown("")
 st.markdown("")
 
 
-button_col1, button_col2 = st.columns(2)
-with button_col1:
-                if st.button("Public health"):
-                    st.page_link("W1943544DPLC-CW/Public_Health.py", label="Go to Public_Health", icon="1️⃣")
-                   # st.switch_page("//Pages//Public_Health.py")
-with button_col2:
-                if st.button("Ecosystem"):
-                     st.switch_page("//Pages//Ecosystem.py")
+#button_col1, button_col2 = st.columns(2)
+#with button_col1:
+#                if st.button("Public health"):
+ #                   st.switch_page("//Pages//Public_Health.py")
+#with button_col2:
+ #               if st.button("Ecosystem"):
+ #                    st.switch_page("//Pages//Ecosystem.py")
 
-
-
-
-st.markdown("")
-st.markdown("")
 
 
 st.subheader("Countries having Wastewater discharged without treatment")
 st.map(Mod_data)
 
-
 st.markdown("")
-
 
 st.subheader("Histogram of Total Wastewater discharged without treatment")
 fig2 = px.histogram(data_frame=Mod_data, x="Year", y ="Total Wastewater" )
 st.write(fig2)
 
+st.markdown("")
 
+st.title("Public Health impacted due to Wastewater")
+st.markdown("---------------------------------------------------------------")
+
+
+
+#Create a sidebar for year dropdown selection
+with st.sidebar:
+     st.title('Water - contaminants and levels Dashboard')
+    
+     years = list(Mod_data.Year.unique())[::-1]
+    
+     selected_year = st.selectbox('Select year to view', years)
+     df_year_selected = Mod_data[Mod_data.Year == selected_year]
+     df_year_selected_sorted = df_year_selected.sort_values(by="Entity", ascending=False)
+
+
+
+
+st.markdown("It was found that by increasing the volume of urban wastewater premature deaths rise by approximately 0.844, highlighting significant health risks from inadequate wastewater management.")
+st.markdown("")
+
+
+
+st.subheader("Correlation between Premature Deaths and Total Wastewater discharged without treatment for selected year")
+st.markdown("")
+st.line_chart(df_year_selected, x="Entity", y=["Total Wastewater","Premature_Death_Count" ], color=["#FF0000", "#0000FF"]  # Optional
+)
+st.markdown("")
+st.markdown("")
+
+
+st.subheader("Correlation between Premature Deaths and Urban Wastewater discharged without treatment for selected year")
+st.markdown("")
+st.line_chart(df_year_selected, x="Entity", y=["Urban wastewater, all sources, discharged without treatment(million m3)","Premature_Death_Count" ], color=["#FF0000", "#0000FF"]  # Optional
+)
+st.markdown("")
+st.markdown("")
+
+st.subheader("Correlation between Premature Deaths and Agricultural Wastewater discharged without treatment for selected year")
+st.markdown("")
+st.line_chart(df_year_selected, x="Entity", y=["Agricultural (incl. forestry + fisheries) wastewater, all sources, direct discharges(million m3)","Premature_Death_Count" ], color=["#FF0000", "#0000FF"]  # Optional
+)
+st.markdown("")
+st.markdown("")
+
+st.subheader("Correlation between Premature Deaths and Agricultural Wastewater discharged without treatment for selected year")
+st.markdown("")
+st.line_chart(df_year_selected, x="Entity", y=["Industrial wastewater, all sources, discharged without treatment(million m3)","Premature_Death_Count" ], color=["#FF0000", "#0000FF"]  # Optional
+)
+
+
+st.subheader("Correlation between Premature Deaths and Total Wastewater discharged without treatment from 2015-2019")
+st.markdown("")
+st.scatter_chart(Mod_data, x="Entity", y=["Total Wastewater","Premature_Death_Count" ], color=["#FF0000", "#0000FF"]  # Optional
+)
+
+st.markdown("")
+st.markdown("")
+st.markdown("")
+
+
+st.title("Ecosystem impacted due to Wastewater")
+st.markdown("---------------------------------------------------------------")
+
+st.subheader("Correlation between Total Wastewater discharged without treatment and Total discharges to Inland waters for selected year")
+st.markdown("")
+st.line_chart(df_year_selected, x="Entity", y=["Total Wastewater","Total discharges to Inland waters(million m3)" ], color=["#FF0000", "#0000FF"]  # Optional
+)
+st.markdown("")
+st.markdown("")
+
+
+st.subheader("Correlation between Total Wastewater discharged without treatment and Total discharges to the sea for selected year")
+st.markdown("")
+st.line_chart(df_year_selected, x="Entity", y=["Total Wastewater","Total discharges to the sea(million m3)" ], color=["#FF0000", "#0000FF"]  # Optional
+)
+st.markdown("")
+st.markdown("")
+
+st.subheader("Correlation between  Total Wastewater, Total discharges to the sea and Total discharges to Inland waters from 2015-2019")
+st.markdown("")
+st.scatter_chart(Mod_data, x="Entity", y=["Total Wastewater","Total discharges to the sea(million m3)","Total discharges to Inland waters(million m3)" ], color=["#FF0000", "#0000FF","#0CC80C"]  # Optional
+)
 
 st.markdown("")
 
 
 st.subheader('Thank you for visiting :smiley:')
-
 
